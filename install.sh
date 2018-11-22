3643c49967b076bcbbbe39a4131f57c6cab1390c
Tsung-Yu HSIEH <i1007673219@gmail.com>
2018-11-22 21:14:13 +0800

    2018-11-22 21:14

--- install.sh
+++ install.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 # url
+DOWNLOADS=$HOME/Downloads
 CHROME_URL=https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-GO_URL=https://dl.google.com/go/go1.11.linux-amd64.tar.gz
 VIM_URL=https://github.com/vim/vim.git
 
 # download
-cd $HOME/Downloads
-wget $CHROME_URL
-wget $GO_URL
-cd -
+wget $CHROME_URL -P $DOWNLOADS
 
 # apt
 apt update -y
 apt install git ssh xclip vim \
             ibus-chewing ibus-mozc \
             smplayer transmission htop \
-            gcc g++ cmake python-dev python3-dev python3-pip \
+            gcc g++ cmake python-dev python3-dev python3-pip golang \
             zsh powerline autojump tmux \
             lm-sensors colordiff \
             libncurses5-dev libgnome2-dev libgnomeui-dev \
@@ -26,13 +23,5 @@ apt upgrade -y
 apt autoremove -y
 
 # dpkg
-cd $HOME/Downloads
-dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
-cd -
-
-# go
-cd $HOME/Downloads
-tar -C /usr/local -xzf go1.11.linux-amd64.tar.gz
-rm go1.11.linux-amd64.tar.gz
-cd -
+dpkg -i $DOWNLOADS/google-chrome-stable_current_amd64.deb
+rm $DOWNLOADS/Downloads/google-chrome-stable_current_amd64.deb
